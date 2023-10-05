ba6a2701920d3b3e4e7bdb92e87903a4a7000b31
nick062333 <nick062333@gmail.com>
2023-10-05 10:43:23 +0800

    add user service

---
+++ src/DataService/Service/UserService.cs
@@ -0,0 +1,10 @@
+namespace DataService.Repositories
+{
+    public class UserService : IUserService
+    {
+        public Task AddUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
