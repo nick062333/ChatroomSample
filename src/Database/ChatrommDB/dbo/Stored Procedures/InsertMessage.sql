09f720f0485992b609129bdfa3fb6e4a515afe40
nick062333 <nick062333@gmail.com>
2023-10-10 22:49:52 +0800

    add database

---
+++ src/Database/ChatrommDB/dbo/Stored Procedures/InsertMessage.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[InsertMessage]
+    @ChatroomId uniqueidentifier,
+    @SendUserId bigint,
+    @Message VARCHAR(max),
+    @SendTime datetime
+AS
+BEGIN
+    INSERT INTO [dbo].[Message]
+               ([ChatroomId]
+               ,[SendUserId]
+               ,[Status]
+               ,[Message]
+               ,[SendTime])
+         VALUES
+               (@ChatroomId
+               ,@SendUserId
+               ,1
+               ,@Message
+               ,@SendTime)
+END;
---
+++ src/Database/ChatrommDB/dbo/Tables/Message.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Message] (
+    [Id]         BIGINT           IDENTITY (1, 1) NOT NULL,
+    [ChatroomId] UNIQUEIDENTIFIER NOT NULL,
+    [Status]     TINYINT          CONSTRAINT [DF_Message_Status] DEFAULT ((0)) NOT NULL,
+    [Message]    VARCHAR (MAX)    NOT NULL,
+    [SendUserId] BIGINT           NOT NULL,
+    [SendTime]   DATETIME         NOT NULL,
+    CONSTRAINT [PK_Message] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
