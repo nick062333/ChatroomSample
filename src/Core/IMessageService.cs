1e845b61ce35bd9ce064226115f7432481e9693b
nick062333 <nick062333@gmail.com>
2023-10-16 13:45:01 +0800

    modfiy summary

--- src/Core/IMessageService.cs
+++ src/Core/IMessageService.cs
@@ -1,4 +1,5 @@
 using DataClass.DTOs;
+using DataClass.Enums;
 
 namespace Core
 {
@@ -7,30 +8,29 @@ namespace Core
         /// <summary>
         /// 取得訊息紀錄列表總筆數
         /// </summary>
-        /// <param name="getMessageLogTotalCountRequest"></param>
+        /// <param name="getMessageLogTotalCountRequest">查詢參數</param>
         /// <returns></returns>
         public Task<GetMessageLogTotalCountResponse> GetMessageLogTotalCountAsync(GetMessageLogTotalCountRequest getMessageLogTotalCountRequest);
 
         /// <summary>
         /// 取得訊息紀錄
         /// </summary>
-        /// <param name="getMessageLogListRequest"></param>
+        /// <param name="getMessageLogListRequest">查詢參數</param>
         /// <returns></returns>
         public Task<GetMessageLogListResponse> GetMessageLogListAsync(GetMessageLogListRequest getMessageLogListRequest);
 
         /// <summary>
         /// 接收訊息後的處理程序
         /// </summary>
-        /// <param name="receiveMessageProcessRequest"></param>
+        /// <param name="receiveMessageProcessRequest">查詢參數</param>
         /// <returns></returns>
         public Task ReceiveMessageProcessAsync(ReceiveMessageProcessRequest receiveMessageProcessRequest);
 
         /// <summary>
         /// 取得Id範圍內的訊息
         /// </summary>
-        /// <param name="groupId">群組代碼</param>
-        /// <param name="startId">Id起始值</param>
+        /// <param name="request">查詢參數</param>
         /// <returns></returns>
-        public Task<GetMessageLogListByIdRangeRequest> GetMessageLogListByIdRangeAsync(Guid groupId, int startId);
+        public Task<GetMessageLogListByIdRangeResponse> GetMessageLogListByIdRangeAsync(GetMessageLogListByIdRangeRequest request);
     }
 }
