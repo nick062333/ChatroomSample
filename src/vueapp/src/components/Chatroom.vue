86b2023c425a91cb420a6d45101d011e8523a4fa
nick062333 <nick062333@gmail.com>
2023-10-17 14:12:22 +0800

    modify chatroom ui

--- src/vueapp/src/components/Chatroom.vue
+++ src/vueapp/src/components/Chatroom.vue
@@ -363,13 +363,13 @@
                 <div class="chat">
                     <div class="chat-header clearfix">
                         <div class="row">
-                            <div class="col-lg-6">
+                            <div class="col-lg-6" v-if="ChatroomData">
                                 <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
                                     <!-- <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar"> -->
                                     <img src="..\images\default.png" alt="avatar">
                                 </a>
                                 <div class="chat-about">
-                                    <h6 class="m-b-0" v-if="ChatroomData">{{ ChatroomData.UserName }}</h6>
+                                    <h6 class="m-b-0" >{{ ChatroomData.UserName }}</h6>
                                     <!-- <small>Last seen: 2 hours ago</small> -->
                                 </div>
                             </div>
